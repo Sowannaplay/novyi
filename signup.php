d28d9523bdc61b843ab8e32ecc219450cf6204fb
Sowannaplay <stronghol@ukr.net>
2019-04-19 19:05:34 +0300

    dodelat email cheker

--- signup.php
+++ signup.php
@@ -1,6 +1,8 @@
 <?php
 
 require "mysqlconnection.php";
+require "validation.php";
+$conn = dbcon();
 
 if (isset($_GET['login']))
 {
@@ -11,26 +13,32 @@ if (isset($_GET['signup']))
 {
     header ('Location: http://localhost/');
 }
+    $checkquery="SELECT email FROM user";
+    $checker=mysqli_fetch_assoc(mysqli_query($conn, $checkquery));
+    if (email_validation($_POST['email'], $checker)==true) {
 
+        if (isset($_POST['register'])) {
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $salt = file_get_contents("salt.txt");
+            $hashed = hash('sha256', $password . $salt);
+            $query = "INSERT INTO user (firstname, lastname, email, password) VALUES ('$firstname','$lastname', '$email', '$password')";
+            $result = mysqli_query($conn, $query);
 
-if (isset($_POST['register'])) {
-    $user[0] = $_POST['firstname'];
-    $user[1] = $_POST['lastname'];
-    $user[2] = $_POST['email'];
-    $user[3] = $_POST['password'];
-    $salt = file_get_contents("salt.txt");
-    $hashed = hash('sha256', $user[3] . $salt);
-    $conn = dbcon();
-    $query = "INSERT INTO user (firstname, lastname, email, password) VALUES ('$user[0]','$user[1]', '$user[2]', '$hashed')";
-    $result = mysqli_query($conn, $query);
-
-    if ($result == false) {
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-        echo "Error:" . mysqli_error($conn);
-        exit;
+            if ($result == false) {
+                echo "Error: Unable to connect to MySQL." . PHP_EOL;
+                echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+                echo "Error:" . mysqli_error($conn);
+                exit;
+            }
+            session_start();
+            $_SESSION['user'] = $firstname;
+            header("location: http://localhost/welcome.php");
+        }
     }
-
-}
+    else {
+        echo "soryan bratan";}
 ?>
